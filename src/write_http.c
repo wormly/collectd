3cae300819a383187bdee787cb97d0309e34181e
Rodion Vynnychenko <roddiku@gmail.com>
2015-02-27 19:22:22 +0200

    Fixed segfault when called with -T

--- src/write_http.c
+++ src/write_http.c
@@ -626,7 +626,11 @@ static int wh_config_url (oconfig_item_t *ci) /* {{{ */
         user_data.free_func = wh_callback_free;
         plugin_register_write ("write_http", wh_write, &user_data);
 
-        plugin_register_complex_read  (/* group */ NULL, /* name */ "write_http/wormly_patch_write_now", wh_flush_now, NULL, &user_data);
+        // if collectd was called with -T and no read threads were started, we're not flushing since -T invokes wh_flush manually
+        if (atoi(global_option_get("ReadThreads")) != -1) {
+            DEBUG ("write_http: Will not flush as read plugin since there are no read threads");
+            plugin_register_complex_read  (/* group */ NULL, /* name */ "write_http/wormly_patch_write_now", wh_flush_now, NULL, &user_data);
+        }
 
         return (0);
 } /* }}} int wh_config_url */
